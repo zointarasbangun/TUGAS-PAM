c44a8e43f391b05f647b73b7f1ec41eeb93cd604
zointarasbangun <103173233+zointarasbangun@users.noreply.github.com>
2022-10-04 00:15:59 +0700

    Delete promisesAsync.js

--- promisesAsync.js
+++
@@ -1,8 +0,0 @@
-async function myDisplay() {
-    let myPromise = new Promise(function(resolve) {
-      setTimeout(function() {resolve("I love You !!");}, 3000);
-    });
-    console.log(await myPromise)
-  }
-  
-  myDisplay();
